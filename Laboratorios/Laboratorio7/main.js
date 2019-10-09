82e2a6dfc150e635e7af1171669b00a46359b0c6
mclara09 <00146318@uca.edu.sv>
2019-10-09 10:09:03 -0600

    laboratorio7

---
+++ Laboratorios/Laboratorio7/main.js
@@ -0,0 +1,57 @@
+let  carnet_field= document.querySelector('#carnet_field')
+let schedule_dropdown=document.querySelector('#schedule_drowpdown')
+let late_switch=document.querySelector('#late_switch')
+let submit_btn=document.querySelector('#submit_btn')
+let table_body=document.querySelector('#table_body')
+
+let carnet_regex=new RegExp('[0-9]{8}')
+
+let addstudent =(carnet,schedule,late)=>{
+    let new_row = document.createElement('tr')
+    let datetime = new Date()
+
+    new_row.className = 'table-active'
+    new_row.innerHTML=`<th scope='row'>${carnet}</th>
+    <th> ${schedule}</th>
+    <th> ${datetime.toLocaleTimeString()}</th>
+    <th> ${late}</th>`
+    table_body .appendChild(new_row)
+       
+}
+
+let parseLateSwitch =(value)=>{
+if (value){
+    return 'Tardisimo'
+}
+return 'A Tiempo'
+
+}
+
+submit_btn.addEventListener('click',()=>{
+    
+    let carnet = carnet_field.value
+
+    let schedule=schedule_dropdown.options[schedule_dropdown.selectedIndex].text
+    let late =parseLateSwitch(late_switch.checked)
+    if(carnet_regex.test(carnet)){
+        addstudent(carnet,schedule,late)
+    }
+    else{
+        alert('Formato de carnet no valido')
+    }
+})
+
+carnet_field.addEventListener('keyup',(event)=>{
+let keyCode = event.keyCode
+let carnet =carnet_field.value
+
+if(keyCode==13){
+    submit_btn.click()
+}
+if(carnet_regex.test(carnet)){
+    submit_btn.disabled=false
+}
+else{
+    submit_btn.disabled=true
+}
+})
